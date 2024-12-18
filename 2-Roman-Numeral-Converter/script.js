0410288bf6715e1c2e1f233a00d78e1ace8f909f
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-17 21:12:41 -0500

    feat: :sparkles: Refactor convertBtn click event to improve performance

--- 2-Roman-Numeral-Converter/script.js
+++ 2-Roman-Numeral-Converter/script.js
@@ -4,12 +4,12 @@ const output = document.getElementById('output')
 
 convertBtn.addEventListener('click', (e)=>{
     e.preventDefault()
-    let numberValue = number.value
-    if(!parseInt(numberValue) && numberValue !== "0"){
+    let numberValue = parseInt(number.value)
+    if(!numberValue && numberValue !== 0){
         output.textContent = "Please enter a valid number"
-    }else if(parseInt(numberValue) > 3999){
+    }else if(numberValue > 3999){
         output.textContent = "Please enter a number less than or equal to 3999"
-    }else if(parseInt(numberValue) <= 0){
+    }else if(numberValue <= 0){
         output.textContent = "Please enter a number greater than or equal to 1"
     }
     else{
