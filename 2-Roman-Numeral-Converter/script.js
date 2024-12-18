b7e83b4d8d36af9aee1a36af47c1d9aa9449f8a8
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-17 21:05:09 -0500

    feat: :sparkles: Add Arabic to Roman numeral conversion

--- 2-Roman-Numeral-Converter/script.js
+++ 2-Roman-Numeral-Converter/script.js
@@ -13,6 +13,34 @@ convertBtn.addEventListener('click', (e)=>{
         output.textContent = "Please enter a number greater than or equal to 1"
     }
     else{
-        output.textContent = numberValue
+        output.textContent = arabicToRoman(numberValue)
     }
 })
+
+const nummerals = [
+    { roman: "M", arabic: 1000 },
+    { roman: "CM", arabic: 900 },
+    { roman: "D", arabic: 500 },
+    { roman: "CD", arabic: 400 },
+    { roman: "C", arabic: 100 },
+    { roman: "XC", arabic: 90 },
+    { roman: "L", arabic: 50 },
+    { roman: "XL", arabic: 40 },
+    { roman: "X", arabic: 10 },
+    { roman: "IX", arabic: 9 },
+    { roman: "V", arabic: 5 },
+    { roman: "IV", arabic: 4 },
+    { roman: "I", arabic: 1 }
+];
+
+function arabicToRoman(arabicNum){
+    if(arabicNum > 0){
+        for(const nummeral of nummerals){
+            if(arabicNum >= nummeral.arabic){
+                return nummeral.roman + arabicToRoman(arabicNum-nummeral.arabic)
+            }
+        }
+    }else{
+        return ''
+    }
+}
