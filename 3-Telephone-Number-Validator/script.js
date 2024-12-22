2170c297cc1341695fa46bdc09a436ca95b2cc5a
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-21 22:31:59 -0500

    feat: :sparkles: Add all the required validators for the phone format
    -Matches:
    1 555-555-5555
    1 (555) 555-5555
    1(555)555-5555
    1 555 555 5555
    5555555555
    555-555-5555
    (555)555-5555

--- 3-Telephone-Number-Validator/script.js
+++ 3-Telephone-Number-Validator/script.js
@@ -4,7 +4,9 @@ const clearBtn = document.getElementById('clear-btn')
 const results = document.getElementById('results-div')
 
 const phoneRegexList = [
-    /\d{10}/, //5555555555
+    /^(1\s)?((\d{3})(-|\s)?){2}(\d{4})$/,
+    /^1?\((\d){3}\)(\d){3}-(\d){4}$/,
+    /^1\s\((\d){3}\)\s(\d){3}-(\d){4}$/,
 ]
 
 const validatePhoneNum = phoneNum => phoneRegexList.some(regex => regex.test(phoneNum))
