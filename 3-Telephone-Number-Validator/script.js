583add3b3af6a74f69a40ce6b3fff643b5890456
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-21 21:46:06 -0500

    feat: :sparkles: Add phone number validation functionality
    - Implemented phone number validation using regex.
    - Added dynamic result messages for valid and invalid phone numbers.

--- 3-Telephone-Number-Validator/script.js
+++ 3-Telephone-Number-Validator/script.js
@@ -3,11 +3,29 @@ const checkBtn = document.getElementById('check-btn')
 const clearBtn = document.getElementById('clear-btn')
 const results = document.getElementById('results-div')
 
+const phoneRegexList = [
+    /\d{10}/, //5555555555
+]
+
+const validatePhoneNum = phoneNum => phoneRegexList.some(regex => regex.test(phoneNum))
+
+
 checkBtn.addEventListener('click', e => {
     e.preventDefault()
     const inputValue = input.value
     if (inputValue === '') {
         alert('Please provide a phone number')
+    }
+    else{
+        const result = document.createElement('div')
+        if(validatePhoneNum(inputValue)){
+            result.className = 'result-valid'
+            result.innerText = `Valid US number: ${inputValue}`
+        }else{
+            result.className = 'result-invalid'
+            result.innerText = `Invalid US number: ${inputValue}`
+        }
+        results.appendChild(result)
     }
 
     input.value = ''
